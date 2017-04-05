1cb1096a279f6fe2f091775ee4a587d77482e4db
PixelFireblade <joshuamoses@outlook.com>
2017-04-05 16:57:06 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
- var x = prompt("How much would you like to be your maximum for your random number?", "0");
- var num1 = parseInt(x);
 $ ( document ).ready(function(){
   $("#findNumber").click(function() {
+    var x = prompt("How much would you like to be your maximum for your random number?", "0");
+    var num1 = parseInt(x);
     var randomNumber = Math.floor(Math.random() * (num1 - 1 + 1)) + 1;
     console.log(randomNumber);
     $("p").text(randomNumber);
