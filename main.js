dad3c5c7c4ef446e8a4ceef8c67b40579e8a3085
PixelFireblade <joshuamoses@outlook.com>
2017-04-05 12:28:24 +1000

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 $ ( document ).ready(function(){
   $("#findNumber").click(function() {
-    var randomNumber = Math.floor(Math.random() * (1000000000 - 1 + 1)) + 1;
+    var randomNumber = Math.floor(Math.random() * (10000 - 1 + 1)) + 1;
     console.log(randomNumber);
     $("p").text(randomNumber);
  })
