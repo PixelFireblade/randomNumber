f39bd83bc7505c5814b281fe2916e16af8ae26a4
PixelFireblade <joshuamoses@outlook.com>
2017-04-05 11:20:49 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 
 
 $ ( document ).ready(function(){
-  $('#choiceOptionHistory').change(changeOptions);
-  $('#randomOptionHistory').change(changeOptions2);
   $("#findNumber").click(function() {
     var randomNumber = Math.floor(Math.random() * (1000000000 - 1 + 1)) + 1;
     console.log(randomNumber);
