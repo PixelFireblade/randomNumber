1a68ce17f63d96139d30c43a09b68048b1a35abe
PixelFireblade <joshuamoses@outlook.com>
2017-05-24 10:54:09 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 $ ( document ).ready(function(){
   $("#findNumber").click(function() {
+    var nums = [];
     var x = $('#minimumId').val()
     var y = $('#maximumId').val()
     var num1 = parseInt(x);
     var num2 = parseInt(y);
+    
     var randomNumber = Math.floor(Math.random() * (num2 - num1 + 1)) + num1;
+    
+    
+    while $.inArray(randomNumber, nums){
+       var randomNumber = Math.floor(Math.random() * (num2 - num1 + 1)) + num1;
+    }
+    else {
+       nums.push(randomNumber);
+    }
+   
+    
+    
+    
     console.log(randomNumber);
     $("h2").text(randomNumber);
  })
