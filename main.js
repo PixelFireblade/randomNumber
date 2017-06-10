3823478c75369190f83d6dd563bd75db35b5cb70
PixelFireblade <joshuamoses@outlook.com>
2017-06-10 16:48:53 +1000

    Create main.js

--- main.js
+++ main.js
@@ -1,43 +1,19 @@
 $ ( document ).ready(function(){
+  var num1 = 1;
+  var num2 = 10000;
   $("#findNumber").click(function() {
     var nums = [];
     var x = $('#minimumId').val()
     var y = $('#maximumId').val()
-    var num1 = parseInt(x);
-    var num2 = parseInt(y);
+    num1 = parseInt(x);
+    num2 = parseInt(y);
     
     var randomNumber = Math.floor(Math.random() * (num2 - num1 + 1)) + num1;
     
-    if ($('#random').prop('checked', true)){
-    
-    //Deselects Opposite Verb, Adjective and Noun
-    $('#choiceVerb').prop('checked', false);
-    $('#choiceAdjective').prop('checked', false);
-    $('#choiceNoun').prop('checked', false);
-    
-    //Deselects Choice
-    $('#random').prop('checked', true);
-    $('#choiceOption').prop('checked', false);
-    
-    //Undisables Verb, Adjective and Noun
-    $('#choiceVerb').prop('disabled', true);
-    $('#choiceAdjective').prop('disabled', true);
-    $('#choiceNoun').prop('disabled', true);
-    
-    //Disables Opposite Verb, Adjective and Noun
-    $('#randomVerb').prop('disabled', false);
-    $('#randomAdjective').prop('disabled', false);
-    $('#randomNoun').prop('disabled', false);
     
     
   }
-   // while jQuery.inArray(randomNumber, nums){
-   //    var randomNumber = Math.floor(Math.random() * (num2 - num1 + 1)) + num1;
-    //}
-    //else {
-  //     nums.push(randomNumber);
-   // }
-   
+
     
     
     
